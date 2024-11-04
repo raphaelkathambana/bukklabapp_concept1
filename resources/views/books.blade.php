fbc482bd49c89d8e4ed92151f4f84140fbc5fa19
Raphael Kathambana <maya12raph@gmail.com>
2024-11-04 18:07:18 +0300

    add books view and update navigation links

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Dashboard') }}
+            {{ __('Books') }}
         </h2>
     </x-slot>
 
@@ -9,10 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Here you will be able to see a summary of all stats: Books you're readingm, your progress, your streak and call to actions to add a log") }}
+                    {{ __("In this page, you will see a collage of the books you have on your self: to read, reading, paused and dead") }}
                 </div>
             </div>
         </div>
