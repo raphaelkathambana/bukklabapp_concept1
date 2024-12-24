e53a5f8aa4516c5137c6309d217edb7d71b6d163
Raphael Kathambana <maya12raph@gmail.com>
2024-12-24 11:00:51 +0300

    add BookshelfCard component for displaying bookshelf details

---
+++ resources/views/components/bookshelf-card.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class="flex flex-col rounded-2xl w-64 bg-[#ffffff] shadow-xl">
+        <div class="flex flex-col p-8">
+            <div class="text-2xl font-bold  text-center text-[#374151] pb-6">{{ $title }}</div>
+            <div class="flex justify-end pt-6">
+                <button
+                    class="bg-primary
+                -600 text-[#ffffff] w-full font-bold text-base  p-3 rounded-lg hover:bg-primary-400 active:scale-95 transition-transform transform">Jump
+                    in</button>
+
+            </div>
+        </div>
+    </div>
+</div>
