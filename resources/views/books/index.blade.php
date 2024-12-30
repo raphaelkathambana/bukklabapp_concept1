77b707a5b6e69c768d24860349fc160b2e73afaf
Raphael Kathambana <maya12raph@gmail.com>
2024-12-31 02:42:57 +0300

    refactor books index view for improved styling and add search link

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,26 +1,31 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+        <h2 class="text-xl font-semibold leading-tight text-text-800 dark:text-text-200">
             {{ __('Books') }}
+            <a href="{{ route('books.search') }}" class="text-primary-500 dark:text-primary-300 hover:underline">
+                {{ __('Search Books') }}
+            </a>
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __('In this page, you will see a collage of the books you have on your self: to read, reading, paused and dead') }}
+            <div class="overflow-hidden bg-background-50 dark:bg-background-800 shadow-sm sm:rounded-lg">
+                <div class="p-6 text-text-700 dark:text-text-300">
+                    {{ __('In this page, you will see a collage of all the books you have on your shelves') }}
 
                     <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3">
                         @if (isset($books) && is_iterable($books))
                             @foreach ($books['data'] as $book)
-                                <x-book-card :title="$book['title']" :author="$book['author']" :cover="$book['cover_image']" :language="$book['language']" />
+                                <x-book-card :title="$book['title']" :author="$book['author']" :cover="$book['cover_image']" :language="$book['language']"
+                                    :id="$book['id']" />
                             @endforeach
                         @else
-                            <p>No books available.</p>
+                            <p class="text-text-600 dark:text-text-400">{{ __('No books available.') }}</p>
                         @endif
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </x-app-layout>
