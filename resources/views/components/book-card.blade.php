8e2e9b9452232c0473778940a727e53549d98cb8
Raphael Kathambana <maya12raph@gmail.com>
2024-12-31 02:43:24 +0300

    refactor book card component for improved layout and dynamic cover image

--- resources/views/components/book-card.blade.php
+++ resources/views/components/book-card.blade.php
@@ -1,15 +1,16 @@
 <div class="flex flex-col rounded-2xl w-64 bg-[#ffffff] shadow-xl">
     <!--[-->
-    <figure class="flex items-center justify-center rounded-2xl">
-        <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" class="rounded-t-2xl">
+    <figure class="flex items-center justify-center rounded-2xl overflow-hidden">
+        <img src="{{ $cover }}" alt="Card Preview" class="rounded-t-2xl w-full object-cover">
     </figure>
     <!--]-->
-    <div class="flex flex-col p-8">
-        <div class="text-2xl font-bold  text-center text-[#374151] pb-6">{{ $title }}</div>
-        <div class="text-base text-[#6B7280] pb-6">{{ $author }}</div>
+    <div class="flex flex-col p-6">
+        <div class="text-2xl font-bold text-center text-[#374151] pb-4 break-words">{{ $title }}</div>
+        <div class="text-base text-[#6B7280] pb-4 break-words">{{ $author }}</div>
         <!--[-->
-        <div class="flex justify-end pt-6">
-            <button class="bg-primary-600 text-[#ffffff] w-full font-bold text-base  p-3 rounded-lg hover:bg-primary-400 active:scale-95 transition-transform transform">Jump in</button>
+        <div class="flex justify-end pt-4">
+            <a class="bg-primary-600 text-[#ffffff] w-full font-bold text-base p-3 rounded-lg hover:bg-primary-400 active:scale-95 transition-transform transform text-center"
+                href="{{ route('books.show', $id) }}">Jump in</a>
         </div>
         <!--]-->
     </div>
